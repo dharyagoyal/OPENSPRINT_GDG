f077674d58fcd08f25ae037667cb6f9097ce8c6c
Dharya Goyal <151448593+dharyagoyal@users.noreply.github.com>
2026-04-18 11:31:24 +0530

    added circle area

--- area.py
+++ area.py
@@ -1,3 +1,8 @@
+import math
+
 side = int(input("Enter side of square : "))
-area = side * side
-print(area)
+areasq = side * side
+print(areasq)
+radius = int(input("Enter radius of circle : "))
+areacircle = math.pi * radius * radius
+print(areacircle)
